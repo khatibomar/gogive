b58c4a3ff0f3fbb13c3b0869f7950a367a0d2cc2
khatibomar <elkhatibomar@outlook.com>
2022-01-12 17:28:36 +0200

    add validation on queries

--- cmd/api/items.go
+++ cmd/api/items.go
@@ -186,8 +186,9 @@ func (app *application) listItemsHandler(w http.ResponseWriter, r *http.Request)
 	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 
 	input.Filters.Sort = app.readString(qs, "sort", "id")
+	input.Filters.SortSafelist = []string{"id", "name", "-id", "-name"}
 
-	if !v.Valid() {
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
