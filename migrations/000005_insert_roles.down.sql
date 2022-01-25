b335209e02eb3a304e72c09200da5e3104a74bae
khatibomar <elkhatibomar@outlook.com>
2022-01-25 18:14:58 +0200

    fix logic of getting the roles ids

--- migrations/000005_insert_roles.down.sql
+++ migrations/000005_insert_roles.down.sql
@@ -1,10 +1,5 @@
 ALTER TABLE users ALTER COLUMN role_id DROP NOT NULL;
 
-UPDATE users SET role_id=NULL where role_id IN (1,2,3);
+UPDATE users SET role_id=NULL where role_id IN (select role_id from users limit 3);
 
-DELETE FROM roles
-	WHERE role_id=1;
-DELETE FROM roles
-	WHERE role_id=2;
-DELETE FROM roles
-	WHERE role_id=3;
+DELETE FROM roles WHERE role_id IN (select role_id from users limit 3);
--- migrations/000005_insert_roles.up.sql
+++ migrations/000005_insert_roles.up.sql
@@ -1,9 +1,8 @@
-INSERT INTO public.roles(
-	role_name, role_description)
-	VALUES ('user', 'for normal user'),
+INSERT INTO roles(role_name, role_description) VALUES
+	('user', 'for normal user'),
 	('admin','for admins'),
 	('analytic','for analytics');
 commit;
 
-UPDATE users SET role_id=1 WHERE role_id IS NULL;
+UPDATE users SET role_id=(select role_id from roles limit 1) WHERE role_id IS NULL;
 alter table users alter column role_id set not null;
