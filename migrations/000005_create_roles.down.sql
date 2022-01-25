18dfcfc2566b246160581dd9a8b5bdf5a999f176
khatibomar <elkhatibomar@outlook.com>
2022-01-25 14:27:42 +0200

    add roles migartions

---
+++ migrations/000005_create_roles.down.sql
@@ -0,0 +1,6 @@
+DELETE FROM roles
+	WHERE role_id=1;
+DELETE FROM roles
+	WHERE role_id=2;
+DELETE FROM roles
+	WHERE role_id=3;
---
+++ migrations/000005_create_roles.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.roles(
+	role_name, role_description)
+	VALUES ('user', 'for normal user'),
+	('admin','for admins'),
+	('analytic','for analytics');
